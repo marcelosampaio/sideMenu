8791e18b9c41f3c6ffc780fa3da906cf788b8d32
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-07-25 14:09:27 -0300

    Initial Commit

---
+++ sideMenu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sideMenu
+//
+//  Created by Marcelo Sampaio on 7/25/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ sideMenu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sideMenu
+//
+//  Created by Marcelo Sampaio on 7/25/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
